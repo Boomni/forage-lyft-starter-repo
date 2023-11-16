4782a2b44fa17d78ce3abf740a65469ad2a79d10
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-11-16 21:43:10 +0100

    Refactored the codebase to reflect the new UML design for the lyft car rental servicing

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -4,7 +4,7 @@ from car import Car
 
 
 class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
+    def __init__(self, warning_light_is_on):
         super().__init__(last_service_date)
         self.warning_light_is_on = warning_light_is_on
 
